746c77642b76f4980536d6241eb28b47c195d160
Carlos Alvarez <carlos.alvarez@bdguidance.com>
2017-07-25 07:00:55 +0000

    first commit

---
+++ sensor.js
@@ -0,0 +1,44 @@
+module.exports = function(RED) {
+
+    var sense = require('ds18b20');
+
+    function DS18B20Node(config) {
+        RED.nodes.createNode(this, config);
+        this.sensorid = config.sensorid;
+        this.repeat = config.repeat;
+        // user input is in seconds, this is conversion to miliseconds
+        this.timer = config.timer * 1000;
+        var node = this;
+
+        var readSensor = function() {
+            //node.log("reading a sensor with id=" + node.sensorid);
+            // TODO error handling
+            sense.temperature(node.sensorid, function(err, value) {
+                var msg = { payload: value };
+                node.send(msg);
+            });
+        }
+
+        this.on("close", function() {
+            clearInterval(this.tout);
+        });
+
+        if (node.repeat)
+          node.tout = setInterval(readSensor, node.timer);
+
+        this.on('input', readSensor);
+
+    }
+
+    RED.nodes.registerType("sensor-ds18b20", DS18B20Node);
+
+    RED.httpAdmin.get('/sensors/1wire',function(req,res) {
+        // TODO how to handle this credential thing?
+        //var credentials = RED.nodes.getCredentials(req.params.id);
+        //if (credentials) {
+        sense.sensors(function(err, ids) {
+            // TODO error handling
+            res.send(JSON.stringify(ids));
+        });
+    });
+}
